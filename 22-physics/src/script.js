6c91906b911f59cf8d42dc6b771155e0436d9278
Tikhon Belousko <tikhon.belousko@gmail.com>
2022-08-13 15:24:58 +0300

    Update 22.

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import * as CANNON from 'cannon-es'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
@@ -7,6 +8,28 @@ import * as dat from 'lil-gui'
  * Debug
  */
 const gui = new dat.GUI()
+
+const debugObject = {}
+
+debugObject.createSphere = () => {
+  const position = {
+    x: Math.random() - 0.5,
+    y: Math.random() - 0.5 + 2,
+    z: Math.random() - 0.5,
+  }
+  createSphere(Math.random() * 0.5, position)
+}
+gui.add(debugObject, 'createSphere')
+
+debugObject.createBox = () => {
+  const position = {
+    x: Math.random() - 0.5,
+    y: Math.random() - 0.5 + 2,
+    z: Math.random() - 0.5,
+  }
+  createBox(Math.random(), Math.random(), Math.random(), position)
+}
+gui.add(debugObject, 'createBox')
 
 /**
  * Base
@@ -17,6 +40,20 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+// World
+
+const world = new CANNON.World({
+  gravity: new CANNON.Vec3(0, -9.82, 0),
+})
+
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(defaultMaterial, defaultMaterial, {
+  friction: 0.1,
+  restitution: 0.7,
+})
+
+world.addContactMaterial(defaultContactMaterial)
+
 /**
  * Textures
  */
@@ -24,46 +61,95 @@ const textureLoader = new THREE.TextureLoader()
 const cubeTextureLoader = new THREE.CubeTextureLoader()
 
 const environmentMapTexture = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.png',
-    '/textures/environmentMaps/0/nx.png',
-    '/textures/environmentMaps/0/py.png',
-    '/textures/environmentMaps/0/ny.png',
-    '/textures/environmentMaps/0/pz.png',
-    '/textures/environmentMaps/0/nz.png'
+  '/textures/environmentMaps/0/px.png',
+  '/textures/environmentMaps/0/nx.png',
+  '/textures/environmentMaps/0/py.png',
+  '/textures/environmentMaps/0/ny.png',
+  '/textures/environmentMaps/0/pz.png',
+  '/textures/environmentMaps/0/nz.png',
 ])
 
 /**
- * Test sphere
+ * Utils
  */
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(0.5, 32, 32),
-    new THREE.MeshStandardMaterial({
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture,
-        envMapIntensity: 0.5
-    })
-)
-sphere.castShadow = true
-sphere.position.y = 0.5
-scene.add(sphere)
+const objectsToUpdate = []
+
+const sphereGeometry = new THREE.SphereGeometry(1, 32, 32)
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5,
+})
+
+const createSphere = (radius, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(sphereGeometry, material)
+  mesh.castShadow = true
+  mesh.position.copy(position)
+  mesh.scale.set(radius, radius, radius)
+  scene.add(mesh)
+
+  // Cannon.js body
+  const body = new CANNON.Body({
+    mass: 1,
+    shape: new CANNON.Sphere(radius),
+    material: defaultMaterial,
+  })
+
+  body.position.copy(position)
+  world.addBody(body)
+
+  objectsToUpdate.push({ mesh, body })
+}
+
+const createBox = (width, height, depth, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(boxGeometry, material)
+  mesh.castShadow = true
+  mesh.position.copy(position)
+  mesh.scale.set(width, height, depth)
+  scene.add(mesh)
+
+  // Cannon.js body
+  const body = new CANNON.Body({
+    mass: 1,
+    shape: new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth / 2)),
+    material: defaultMaterial,
+  })
+
+  body.position.copy(position)
+  world.addBody(body)
+
+  objectsToUpdate.push({ mesh, body })
+}
 
 /**
  * Floor
  */
 const floor = new THREE.Mesh(
-    new THREE.PlaneGeometry(10, 10),
-    new THREE.MeshStandardMaterial({
-        color: '#777777',
-        metalness: 0.3,
-        roughness: 0.4,
-        envMap: environmentMapTexture,
-        envMapIntensity: 0.5
-    })
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: '#777777',
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture,
+    envMapIntensity: 0.5,
+  }),
 )
 floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
+floor.rotation.x = -Math.PI * 0.5
 scene.add(floor)
+
+const floorBody = new CANNON.Body({
+  mass: 0,
+  shape: new CANNON.Plane(),
+  material: defaultMaterial,
+})
+
+floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI * 0.5)
+world.addBody(floorBody)
 
 /**
  * Lights
@@ -75,10 +161,10 @@ const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
 directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.left = -7
 directionalLight.shadow.camera.top = 7
 directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
+directionalLight.shadow.camera.bottom = -7
 directionalLight.position.set(5, 5, 5)
 scene.add(directionalLight)
 
@@ -86,23 +172,22 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -110,7 +195,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(- 3, 3, 3)
+camera.position.set(-3, 3, 3)
 scene.add(camera)
 
 // Controls
@@ -121,7 +206,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
@@ -132,19 +217,29 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let previousElapsedTime = 0
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
+  const timeSinceLastCalled = elapsedTime - previousElapsedTime
+  previousElapsedTime = elapsedTime
 
-    // Update controls
-    controls.update()
+  world.step(1 / 60, timeSinceLastCalled)
 
-    // Render
-    renderer.render(scene, camera)
+  // Update objects
+  objectsToUpdate.forEach(({ mesh, body }) => {
+    mesh.position.copy(body.position)
+    mesh.quaternion.copy(body.quaternion)
+  })
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Update controls
+  controls.update()
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
