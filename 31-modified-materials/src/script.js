34d7b0856e44ebb8f63e5d7bd502c5962b864ab3
Tikhon Belousko <tikhon.belousko@gmail.com>
2022-08-20 20:06:49 +0300

    Add rotating head to lesson 31.

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -26,30 +26,27 @@ const cubeTextureLoader = new THREE.CubeTextureLoader()
 /**
  * Update all materials
  */
-const updateAllMaterials = () =>
-{
-    scene.traverse((child) =>
-    {
-        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial)
-        {
-            child.material.envMapIntensity = 1
-            child.material.needsUpdate = true
-            child.castShadow = true
-            child.receiveShadow = true
-        }
-    })
+const updateAllMaterials = () => {
+  scene.traverse(child => {
+    if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+      child.material.envMapIntensity = 1
+      child.material.needsUpdate = true
+      child.castShadow = true
+      child.receiveShadow = true
+    }
+  })
 }
 
 /**
  * Environment map
  */
 const environmentMap = cubeTextureLoader.load([
-    '/textures/environmentMaps/0/px.jpg',
-    '/textures/environmentMaps/0/nx.jpg',
-    '/textures/environmentMaps/0/py.jpg',
-    '/textures/environmentMaps/0/ny.jpg',
-    '/textures/environmentMaps/0/pz.jpg',
-    '/textures/environmentMaps/0/nz.jpg'
+  '/textures/environmentMaps/0/px.jpg',
+  '/textures/environmentMaps/0/nx.jpg',
+  '/textures/environmentMaps/0/py.jpg',
+  '/textures/environmentMaps/0/ny.jpg',
+  '/textures/environmentMaps/0/pz.jpg',
+  '/textures/environmentMaps/0/nz.jpg',
 ])
 environmentMap.encoding = THREE.sRGBEncoding
 
@@ -67,28 +64,115 @@ mapTexture.encoding = THREE.sRGBEncoding
 const normalTexture = textureLoader.load('/models/LeePerrySmith/normal.jpg')
 
 // Material
-const material = new THREE.MeshStandardMaterial( {
-    map: mapTexture,
-    normalMap: normalTexture
+const material = new THREE.MeshStandardMaterial({
+  map: mapTexture,
+  normalMap: normalTexture,
 })
+
+const shaderUniforms = {
+  uTime: { value: 0 },
+}
+
+material.onBeforeCompile = shader => {
+  shader.uniforms.uTime = shaderUniforms.uTime
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+      uniform float uTime;
+
+      mat2 getRotationMatrix2D(float angle) {
+        return mat2(
+          cos(angle), -sin(angle),
+          sin(angle), cos(angle)
+        );
+      }
+    `,
+  )
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <beginnormal_vertex>',
+    `
+      #include <beginnormal_vertex>
+
+      float angle = position.y * 0.4 + uTime;
+      mat2 rotationMatrix = getRotationMatrix2D(angle);
+      objectNormal.xz = rotationMatrix * objectNormal.xz;
+    `,
+  )
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+      #include <begin_vertex>
+      transformed.xz = rotationMatrix * transformed.xz;
+    `,
+  )
+}
+
+const depthMaterial = new THREE.MeshDepthMaterial({
+  depthPacking: THREE.RGBADepthPacking,
+})
+
+depthMaterial.onBeforeCompile = shader => {
+  shader.uniforms.uTime = shaderUniforms.uTime
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+      uniform float uTime;
+
+      mat2 getRotationMatrix2D(float angle) {
+        return mat2(
+          cos(angle), -sin(angle),
+          sin(angle), cos(angle)
+        );
+      }
+    `,
+  )
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+      #include <begin_vertex>
+      mat2 rotationMatrix = getRotationMatrix2D(transformed.y * 0.4 + uTime);
+      transformed.xz = rotationMatrix * transformed.xz;
+    `,
+  )
+}
 
 /**
  * Models
  */
-gltfLoader.load(
-    '/models/LeePerrySmith/LeePerrySmith.glb',
-    (gltf) =>
-    {
-        // Model
-        const mesh = gltf.scene.children[0]
-        mesh.rotation.y = Math.PI * 0.5
-        mesh.material = material
-        scene.add(mesh)
+gltfLoader.load('/models/LeePerrySmith/LeePerrySmith.glb', gltf => {
+  // Model
+  const mesh = gltf.scene.children[0]
+  mesh.rotation.y = Math.PI * 0.5
+  mesh.material = material
+  mesh.customDepthMaterial = depthMaterial
+  scene.add(mesh)
 
-        // Update materials
-        updateAllMaterials()
-    }
+  // Update materials
+  updateAllMaterials()
+})
+
+/**
+ * Plane
+ */
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20),
+  new THREE.MeshStandardMaterial({
+    color: '#ffffff',
+    side: THREE.DoubleSide,
+  }),
 )
+
+plane.position.set(0, -5, 5)
+plane.rotation.set(0, Math.PI, 0)
+
+scene.add(plane)
 
 /**
  * Lights
@@ -98,30 +182,29 @@ directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 2, - 2.25)
+directionalLight.position.set(0.25, 2, -2.25)
 scene.add(directionalLight)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -129,7 +212,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(4, 1, - 4)
+camera.position.set(4, 1, -4)
 scene.add(camera)
 
 // Controls
@@ -140,8 +223,8 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-    antialias: true
+  canvas: canvas,
+  antialias: true,
 })
 renderer.shadowMap.enabled = true
 renderer.shadowMap.type = THREE.PCFShadowMap
@@ -157,18 +240,19 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  shaderUniforms.uTime.value = elapsedTime
 
-    // Render
-    renderer.render(scene, camera)
+  // Update controls
+  controls.update()
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
